cc2d84ebb8faadc74325c91ea6c6225c46dd7d7d
A.N <spidernet12@gmail.com>
2021-11-05 19:14:05 +0300

    [feat] Added blog routes

--- routes/pages.r.js
+++ routes/pages.r.js
@@ -15,6 +15,14 @@ router.get("/services", (req, res) => {
   res.render("pages/services");
 });
 
-router.get("/posts/:slug", blogController.postPage);
+router.get("/blog", blogController.blogPosts);
+
+router.get("/blog/topics/:tag", blogController.blogPosts);
+
+router.get("/blog/topics", blogController.blogTopics);
+
+router.get("/blog/archive", blogController.blogArchive);
+
+router.get("/blog/:slug", blogController.postPage);
 
 module.exports = router;
