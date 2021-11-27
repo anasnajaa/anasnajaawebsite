64c6b9bff1622bc0562fa202eb53a99ac541cd5c
A.N <spidernet12@gmail.com>
2021-11-27 15:32:10 +0300

    [feat] Added library and service pages

--- routes/pages.r.js
+++ routes/pages.r.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const blogController = require("../controllers/page/blog.c");
+const pagesController = require("../controllers/page/pages.c");
+const servicesController = require("../controllers/page/service.c");
 
 const router = express.Router();
 
@@ -7,22 +9,16 @@ router.get("/", (req, res) => {
   res.render("pages/index");
 });
 
-router.get("/about", (req, res) => {
-  res.render("pages/about");
-});
+router.get("/library", pagesController.libraryPage);
 
-router.get("/services", (req, res) => {
-  res.render("pages/services");
-});
+router.get("/services", pagesController.servicePage);
+router.get("/services/request", servicesController.serviceRequestForm);
+router.get("/services/faq", servicesController.serviceFaq);
 
 router.get("/blog", blogController.blogPosts);
-
 router.get("/blog/topics/:tag", blogController.blogPosts);
-
 router.get("/blog/topics", blogController.blogTopics);
-
 router.get("/blog/archive", blogController.blogArchive);
-
 router.get("/blog/:slug", blogController.postPage);
 
 module.exports = router;
