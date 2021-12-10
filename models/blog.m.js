867ec8f2862931192323951cbbc316343eeb668e
A.N <spidernet12@gmail.com>
2021-12-10 13:32:18 +0300

    [feat] Improved tags handling logic

--- models/blog.m.js
+++ models/blog.m.js
@@ -72,16 +72,11 @@ exports.getPageBySlug = async (slug) => {
 };
 
 exports.tagsCssResolver = (id) => {
-  if (id === "5f86bc611488fc001eea51ae") {
-    return "blog-red-tag";
-  }
-  if (id === "5f8a7e4a11e139001eb1d5d2") {
-    return "blog-blue-tag";
-  }
-  if (id === "5f7f519ecbab9f001ede0c9b") {
-    return "blog-green-tag";
-  }
-  if (id === "5f8a7f6d11e139001eb1d5d6") {
-    return "blog-purple-tag";
+  switch(id){
+    case "5f86bc611488fc001eea51ae":  return "blog-red-tag";
+    case "5f8a7e4a11e139001eb1d5d2":  return "blog-blue-tag";
+    case "5f7f519ecbab9f001ede0c9b":  return "blog-green-tag";
+    case "5f8a7f6d11e139001eb1d5d6":  return "blog-purple-tag";
+    default:                          return "";
   }
 };
