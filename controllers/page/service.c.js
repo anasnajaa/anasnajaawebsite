166c57ed8987c9492cb3245af2ad1a2f536ddf3d
A.N <spidernet12@gmail.com>
2021-12-10 13:31:58 +0300

    [feat] Added FAQ page

--- controllers/page/service.c.js
+++ controllers/page/service.c.js
@@ -1,9 +1,12 @@
+const blogModel = require("../../models/blog.m");
+
 exports.serviceRequestForm = async (req, res) => {
   return res.render("pages/services-request-form", { pd: {} });
 };
 
 exports.serviceFaq = async (req, res) => {
-  return res.render("pages/services-faq", { pd: {} });
+	const page = await blogModel.getPageBySlug("services-faqs");
+	return res.render("pages/services-faq", { pd: { page } });
 };
 
 exports.serviceVerify = async (req, res) => {
