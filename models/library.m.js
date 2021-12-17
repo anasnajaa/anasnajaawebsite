e315cc858b76fa42ef469b405688515060a03656
A.N <spidernet12@gmail.com>
2021-12-17 19:29:44 +0300

    Update library.m.js

--- models/library.m.js
+++ models/library.m.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema(
-  {
-    title: { type: String, required: false },
-    thumb: { type: String, required: true },
-    date: { type: Date, required: false },
-    url: { type: String, required: true },
-    tags: { type: Array, required: false },
-    type: { type: String, required: true },
-  },
-  { timestamps: false, collection: "library" }
+	{
+		title: { type: String, required: false },
+		thumb: { type: String, required: true },
+		date: { type: Date, required: false },
+		url: { type: String, required: true },
+		tags: { type: Array, required: false },
+		type: { type: String, required: true }
+	},
+	{ timestamps: false, collection: "library" }
 );
 
 module.exports = mongoose.model("library", schema);
