688db4cbf10582e470267001fbb2f324fa5049e9
A.N <spidernet12@gmail.com>
2021-12-17 19:29:14 +0300

    [feat] Applied new error handler

--- controllers/api/other.c.js
+++ controllers/api/other.c.js
@@ -2,18 +2,26 @@ const { v4 } = require("uuid");
 const { contentServer } = require("../../util/awake");
 const { isCaptchaValid } = require("../../util/captcha");
 
-exports.awake = async (req, res) => {
-  const contentServerAwake = await contentServer();
-  res.json({
-    id: v4(),
-    contentServerAwake,
-  });
+exports.awake = async (req, res, next) => {
+	try {
+		const contentServerAwake = await contentServer();
+		res.json({
+			id: v4(),
+			contentServerAwake
+		});
+	} catch (err) {
+		next(err);
+	}
 };
 
-exports.captcha = async (req, res) => {
-  const key = req.query.key;
-  const captchaResult = await isCaptchaValid(key);
-  res.json({
-    isValid: captchaResult,
-  });
+exports.captcha = async (req, res, next) => {
+	try {
+		const key = req.query.key;
+		const captchaResult = await isCaptchaValid(key);
+		res.json({
+			isValid: captchaResult
+		});
+	} catch (err) {
+		next(err);
+	}
 };
