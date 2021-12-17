c59a2c41c68f1f997f91038b280324c6e51e52ab
A.N <spidernet12@gmail.com>
2021-12-17 19:27:38 +0300

    Update email.c.js

--- controllers/api/email.c.js
+++ controllers/api/email.c.js
@@ -1,13 +1,17 @@
 const { sendMail } = require("../../util/mailer");
 
-exports.testEmail = async (req, res) => {
-  const emailResult = await sendMail({
-    to: "spidernet12@gmail.com",
-    subject: "Test Email",
-    text: "Test Email Text",
-    html: "<strong>Test Email HTML</strong>",
-  });
-  res.json({
-    emailResult,
-  });
+exports.testEmail = async (req, res, next) => {
+	try {
+		const emailResult = await sendMail({
+			to: "spidernet12@gmail.com",
+			subject: "Test Email",
+			text: "Test Email Text",
+			html: "<strong>Test Email HTML</strong>"
+		});
+		res.json({
+			emailResult
+		});
+	} catch (err) {
+		next(err);
+	}
 };
